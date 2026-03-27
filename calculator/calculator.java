dbe1a4702bed28c6f6b66458fea2617916000821
Satyamtiwari23 <sttiwari9211@gmail.com>
2026-03-27 13:12:02 +0530

    basic calculator

---
+++ calculator/calculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class calculator {
+        public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Select Operation: ");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        int operation = sc.nextInt();
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+        double result;
+        switch(operation){
+            case 1:
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case 2:
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case 3:
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case 4:
+                if(num2 != 0){
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Division by zero");
+                }
+                break;
+            default:
+                System.out.println("Invalid Operation Selected");
+        }
+        sc.close();
+    }
+    
+}
